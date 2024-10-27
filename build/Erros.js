fd1f0fc74ad151c5ab808b96f3573daa41cc682a
Helder45 <helder.pereira@estudante.ifms.edu.br>
2024-10-27 14:12:09 -0400

    feat: added Errors for user

--- build/Erros.js
+++ build/Erros.js
@@ -1,1 +1,14 @@
-"use strict";
+import chalk from "chalk";
+class CamposVazios extends Error {
+    constructor(msg) {
+        super(msg);
+        this.name = chalk.red("Erro de campos vazios: ");
+    }
+}
+class SenhasDiferentes extends Error {
+    constructor(msg) {
+        super(msg);
+        this.name = chalk.red("Erro de senha: ");
+    }
+}
+export { CamposVazios, SenhasDiferentes };
---
+++ src/Erros.ts
@@ -0,0 +1,18 @@
+import chalk from "chalk";
+
+
+class CamposVazios extends Error {
+    constructor(msg: any) {
+        super(msg);
+        this.name = chalk.red("Erro de campos vazios: ");
+    }
+}
+
+class SenhasDiferentes extends Error {
+    constructor(msg: any) {
+        super(msg);
+        this.name = chalk.red("Erro de senha: ");
+    }
+}
+
+export { CamposVazios, SenhasDiferentes };
